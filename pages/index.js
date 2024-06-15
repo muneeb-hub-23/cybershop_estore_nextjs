f31b6eb445188080648e44a8a307165b45031bab
Muneeb Baig <109512760+muneeb-hub-23@users.noreply.github.com>
2024-06-15 15:09:58 +0500

    initial setup
    will be created using tailwind css

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <h1 className="bg-yellow-400">Hello</h1>
+  </main>
+  )
+}
+
+export default index
